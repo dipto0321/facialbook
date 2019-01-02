e46b2d9461007d9f79a211662d743205e4beafc8
dipto0321 <diptokmk47@gmail.com>
2019-01-02 10:19:17 +0600

    Add test for factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password {'password'}
+    password_confirmation {'password'}
+  end
+
+  factory :invalid_user, class: 'User' do
+    email { }
+    password {'password'}
+    password_confirmation {'password'}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'factories' do
+    it 'should have a valid factory' do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
+
+    it 'should have a invalid factory' do
+      expect(FactoryBot.build(:invalid_user)).not_to be_valid
+    end
+  end
 end
