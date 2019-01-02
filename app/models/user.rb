4158cee34bd945dc31bafc9137943410d70340f0
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-02 13:00:22 +0800

    Create friendship model & user associations
    1. Generate friendship model
    2. Add necessary associations in user model (self joins)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :active_friendships, class_name: "Friendship", foreign_key: :user_id
+  
+  has_many :passive_friendships, class_name: "Friendship", foreign_key: :friend_id
+
+  has_many :added_friends, through: :active_friendships,
+  source: :friend
+
+  has_many :adding_friends, through: :passive_friendships, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_033402) do
+ActiveRecord::Schema.define(version: 2019_01_02_043111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +42,6 @@ ActiveRecord::Schema.define(version: 2019_01_02_033402) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
 end
