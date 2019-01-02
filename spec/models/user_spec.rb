e288d28f6c2f870756df33371de07e2fdfe9d42f
dipto0321 <diptokmk47@gmail.com>
2019-01-02 13:25:30 +0600

    Add test file for user associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe User, type: :model do
       expect(FactoryBot.build(:invalid_user)).not_to be_valid
     end
   end
+
+  describe 'Association' do
+    it { should have_many(:active_friendships).with_foreign_key('user_id').class_name('Friendship') }
+
+    it { should have_many(:passive_friendships).with_foreign_key('friend_id').class_name('Friendship') }
+
+    it { should have_many(:added_friends).through(:active_friendships) }
+
+    it { should have_many(:adding_friends).through(:passive_friendships) }
+  end
 end
