e73d79220787adc423e0fcafc52d723b1d321fb4
dipto0321 <diptokmk47@gmail.com>
2019-01-03 12:07:16 +0600

    Initial commit for pending request feature

--- spec/features/adding_friend_spec.rb
+++ spec/features/adding_friend_spec.rb
@@ -26,5 +26,32 @@ feature 'clicking Add Friend button' do
   it "changes the Add Friend button into Request Sent button" do
     expect(page).to have_selector(:link_or_button, "Request Sent")
   end
+end
 
+feature "checking one's pending requests" do
+  before(:each) do
+    @user = create(:user)
+    @friend = create(:user)
+
+    # First log in
+    visit new_user_session_path
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: "password"
+    click_on("Log in")
+
+    visit user_path(@friend)
+
+    click_on("Add Friend")
+
+    visit root_path
+    click_on("Sign Out")
+    click_on("Sign in")
+    fill_in "Email", with: @friend.email
+    fill_in "Password", with: "password"
+    click_on("Log in")
+  end
+
+  it 'works' do
+    save_and_open_page
+  end
 end
