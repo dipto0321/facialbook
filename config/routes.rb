2ab009afb2a6d063298e7ffaeb1bfdbd90caf061
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-04 13:54:35 +0800

    Add custom route for login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root "static_pages#home"
   devise_for :users
 
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
   resources :users
 
   resources :friendships, only: %i[create destroy]
