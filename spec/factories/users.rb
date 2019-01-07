920560b8cdc40594d6579e91229ac8e657b0ea81
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-07 16:39:00 +0800

    Add profile building in users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,14 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
+
+    after :build do |user|
+      user.profile = Profile.new(user_id: user.id, 
+        first_name: Faker::Name.male_first_name,
+        last_name: Faker::Name.last_name,
+        gender: "male",
+        birthday: Faker::Date.birthday(18,65))
+    end
   end
 
   factory :invalid_user, class: 'User' do
