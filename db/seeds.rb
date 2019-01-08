106704715b6a204c1d616d75f483f0272b068b24
dipto0321 <diptokmk47@gmail.com>
2019-01-08 11:15:39 +0600

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,13 @@ User.create(
   email: 'ryto.verkar@gmail.com',
   password: 'password',
   password_confirmation: 'password',
+  profile_attributes: {
+    first_name: "Ryto",
+    last_name: "Verkar",
+    birthday: Faker::Date.birthday,
+    gender: "male",
+    bio: Faker::Lorem.paragraph(2)
+  }
 )
 
 50.times do
@@ -11,5 +18,27 @@ User.create(
     email: Faker::Internet.email,
     password: 'password',
     password_confirmation: 'password',
+    profile_attributes: {
+      first_name: Faker::Name.male_first_name,
+      last_name: Faker::Name.last_name,
+      birthday: Faker::Date.birthday,
+      gender: "male",
+      bio: Faker::Lorem.paragraph(2)
+    }
+  )
+end
+
+50.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password',
+    profile_attributes: {
+      first_name: Faker::Name.female_first_name,
+      last_name: Faker::Name.last_name,
+      birthday: Faker::Date.birthday,
+      gender: "female",
+      bio: Faker::Lorem.paragraph(2)
+    }
   )
 end
