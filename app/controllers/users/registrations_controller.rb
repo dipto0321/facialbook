dbff746216fcf30c7a793b937c018c52b5b4cc7b
dipto0321 <diptokmk47@gmail.com>
2019-01-08 10:57:28 +0600

    Add edit profile setting

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -54,6 +54,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # The path used after sign up.
   def after_sign_up_path_for(resource)
     # super(resource)
+    debugger
     edit_profile_path(resource.profile.id)
   end
 
