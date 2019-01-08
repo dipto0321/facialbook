97e1143d9bc96963a36d1e93276fd6f2fcddf6c4
dipto0321 <diptokmk47@gmail.com>
2019-01-08 16:13:01 +0600

    Refactor Remove friend button

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,9 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  # attr_reader :find_friendship
+
+  def self.find_friendship(id)
+    Friendship.where("friend_id=? OR user_id=?", id,id)[0]
+  end
 end
