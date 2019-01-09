62657969840c6ec508826ac9513b61ef858e8e9b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-09 12:50:43 +0800

    Fix spacing for all friends and fix pending request view

--- app/controllers/users/all_friends_controller.rb
+++ app/controllers/users/all_friends_controller.rb
@@ -3,6 +3,6 @@
 class Users::AllFriendsController < ApplicationController
   def index
     @user = User.find_by(id: params[:user_id])
-    @all_friends = current_user.friends
+    @all_friends = @user.friends
   end
 end
