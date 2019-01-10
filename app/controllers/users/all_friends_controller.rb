248087c160d1106b8d36376a74093ec844ce276f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-10 12:59:21 +0800

    Write specs for visiting a user's friends pages

--- app/controllers/users/all_friends_controller.rb
+++ app/controllers/users/all_friends_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::AllFriendsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @user = User.find_by(id: params[:user_id])
     @all_friends = @user.friends
--- app/controllers/users/mutual_friends_controller.rb
+++ app/controllers/users/mutual_friends_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::MutualFriendsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @user = User.find_by(id: params[:user_id])
     @mutual_friends = current_user.mutual_friends(@user)
