44569a1ebe235ab605764fe6b224674702b7f1c0
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-11 16:09:30 +0800

    Show posts in logged root page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,11 @@ class Post < ApplicationRecord
   belongs_to :postable, polymorphic: true
   default_scope { order(updated_at: :desc) }
 
-  def self.timeline_post(postable)
+  def self.timeline_posts(postable)
     Post.where("postable_id=?",postable.id)
+  end
+
+  def self.newsfeed_posts(postable)
+    
   end
 end
