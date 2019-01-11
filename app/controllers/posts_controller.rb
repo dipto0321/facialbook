33d8b557907c11a4034fb56561385fdf46e2cdc4
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-11 17:56:35 +0800

    Write create action in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,14 @@ class PostsController < ApplicationController
   end
 
   def create
+    @postable = User.find_by(params[:post][:postable_id])
+    @author = User.find_by(params[:post][:author_id])
+    @post = @postable.build_post(@author)
+    @post.body = params[:post][:body]
+    @post.post_pic = params[:post][:post_pic]
+
+    @post.save
+    redirect_back(fallback_location: root_path)
   end
 
   def update
@@ -19,4 +27,10 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:author_id, :user_id, :post_pic, :body)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   belongs_to :postable, polymorphic: true
   default_scope { order(updated_at: :desc) }
-
+  
   def self.timeline_posts(postable)
     Post.where("postable_id=?",postable.id)
   end
