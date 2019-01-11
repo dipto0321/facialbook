24583a219dd04224866003e7a14fdfadb1060cec
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-11 15:56:28 +0800

    Show timeline posts on user page

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   belongs_to :postable, polymorphic: true
   default_scope { order(updated_at: :desc) }
+
+  def self.timeline_post(postable)
+    Post.where("postable_id=?",postable.id)
+  end
 end
