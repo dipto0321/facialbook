9af7f6c71316bb106d09bfaabf06a0023c84330c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-11 19:31:47 +0800

    Edit user_newsfeed_posts class method further

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,6 @@ class Post < ApplicationRecord
 
   def self.user_newsfeed_posts(postable)
     friend_ids = postable.friends.map {|friend| friend.id}
-    Post.where("postable_id IN (?) OR author_id=?", friend_ids + [postable.id], postable.id)
+    Post.where("postable_id IN (?) OR author_id IN (?)", friend_ids + [postable.id], friend_ids + [postable.id])
   end
 end
