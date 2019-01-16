d4acee8a7c5c973b6d01c13e1d0edf11d43436cc
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-16 15:15:08 +0800

    Add comments create action for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,19 @@
 class CommentsController < ApplicationController
   def edit
   end
+
+  def create
+    @comment = @commentable.comments.build(comment_params)
+    if @comment.save
+      flash[:success] = "Coment submitted"
+    else
+      flash[:danger] = "Comment not submitted"
+    end
+    redirect_to request.referrer
+  end
+
+  private 
+  def comment_params
+    params.require(:comment).permit(:commenter_id, :body, :comment_pic)
+  end
 end
