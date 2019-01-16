3dc8aeac13cdba21195ab28f55b49e2d44111663
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-16 13:46:17 +0800

    Show comments for every post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,6 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :commenter, class_name: 'User'
   has_many :comments, as: :commentable
-
+  default_scope { order(created_at: :desc) }
   validates :body, presence: true
 end
