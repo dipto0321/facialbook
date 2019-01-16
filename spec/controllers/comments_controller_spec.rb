eda889aba017c195a4f7cf791d73b28211f7cc95
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-16 16:43:54 +0800

    Write spec for create action for post cont

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe CommentsController, type: :controller do
+RSpec.describe CommentsController do
 
-  describe "GET #edit" do
-    
+  describe "POST #create" do
+    it "saves to the database" do
+      commenter = create(:user)
+      user_post = create(:user_post,author_id: commenter.id, postable_id: commenter.id)
+      sign_in(commenter)
+      expect{
+        post :create, params: {
+          comment:{
+            commenter_id: commenter.id,
+            body: Faker::Lorem.paragraph(2)
+          }, post_id: user_post.id
+        }
+      }.to change(Comment, :count).by(1)
+    end
   end
-
-
 
 end
