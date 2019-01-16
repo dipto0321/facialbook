49a9462b9edcc2a21a1c1bb47f22a428e7726ab5
dipto0321 <diptokmk47@gmail.com>
2019-01-16 15:45:36 +0600

    Finish spec for comment for post

--- spec/controllers/posts/comments_controller_spec.rb
+++ spec/controllers/posts/comments_controller_spec.rb
@@ -17,6 +17,27 @@ RSpec.describe Posts::CommentsController do
         }
       }.to change(Comment, :count).by(1)
     end
-  end
 
+    context 'instance variable assingment and redirection' do
+      before(:each) do
+        @commenter = create(:user)
+        @user_post = create(:user_post,author_id: @commenter.id, postable_id: @commenter.id)
+        parameters = {params: {
+            comment: attributes_for(:post_comment, commenter_id: @commenter.id), post_id: @user_post.id
+          }
+        }
+        session[:return_to] = root_path
+        sign_in(@commenter)
+        post :create, parameters
+      end
+
+      it 'assigns to @comment' do
+        expect(assigns(:commentable)).to eq(@user_post)
+      end
+
+      it 'redirects successfully' do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
 end
