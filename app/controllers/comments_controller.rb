48e818318e5b8873fff9a95b6efb0b82dcbd3183
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-17 14:41:33 +0800

    Complete update action for post comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,13 @@ class CommentsController < ApplicationController
     session.delete(:return_to)
   end
 
+  def update
+    @comment = Comment.find_by(id: params[:id])
+    @comment.update(comment_params)
+    redirect_to session[:return_to]
+    session.delete(:return_to)
+  end
+
   private 
   def comment_params
     params.require(:comment).permit(:author_id, :body, :comment_pic)
