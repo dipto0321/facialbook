23ee1a14b606361660e8c8849ef1c897466c66f9
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-21 13:56:03 +0800

    Refactor index specs for post likes controller

--- spec/controllers/posts/likes_controller_spec.rb
+++ spec/controllers/posts/likes_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Posts::LikesController do
   describe "GET #index" do
-    before :each do
+    it "assigns to @likes" do
       @user = create(:user)
       @user_post = create(:user_post, author_id: @user.id, postable_id: @user.id)
       @liker = create(:user)
@@ -12,9 +12,6 @@ RSpec.describe Posts::LikesController do
       @like2 = create(:post_like, liker_id: @user.id, likeable_id: @user_post.id)
       sign_in(@user)
       get :index, params:{post_id: @user_post.id}
-    end
-
-    it "assigns to @likes" do
       expect(assigns(:likes)).to eq(@user_post.likes)
     end
 
