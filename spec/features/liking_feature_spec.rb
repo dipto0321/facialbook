c7cb562eb79848195886986b140fae2ade83fcd2
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-21 12:19:37 +0800

    Finish liking feature specs (create likes)

--- spec/features/liking_feature_spec.rb
+++ spec/features/liking_feature_spec.rb
@@ -1,8 +1,34 @@
 require 'rails_helper'
 
-feature "Liking post" do
-  
-end
+feature "liking" do
+  before :each do
+    @user = create(:user)
+    @liker = create(:user)
+    @post = create(:user_post, author_id: @user.id, postable_id: @user.id)
+    @comment = create(:post_comment, commentable_id: @post.id, author_id: @user.id)
+    visit login_path
+    fill_in 'Email', with: @liker.email
+    fill_in 'Password', with: @liker.password
+    click_on('Log in')
+    visit user_path(@user)
+  end
 
-feature "Liking a comment" do
+  context "Liking post" do
+    it "likes the post" do
+      within "div#post-interaction-btns" do
+        click_on "LIKE"
+      end
+      expect(page).to have_selector(:link_or_button, "#{@post.likes.count} like")
+    end
+  end
+
+  context "Liking a comment" do
+    it "likes the post" do
+      within "div.comment-bg" do
+        click_on "Like"
+      end
+      expect(page).to have_selector(:link_or_button, "#{@comment.likes.count} like")
+    end
+  end
+
 end
