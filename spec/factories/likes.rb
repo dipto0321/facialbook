7030d10a300f17bab4dbed59189c170f54be7649
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-01-21 10:26:56 +0800

    Write spec for like model and likes factories

--- spec/factories/likes.rb
+++ spec/factories/likes.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
-  factory :like do
-    likeable { nil }
-    liker { nil }
+  factory :post_like, class: "Like" do
+    likeable_id { 1 }
+    likeable_type { "Post" }
+    liker { 1 }
+  end
+
+  factory :comment_like, class: "Like" do
+    likeable_id { 1 }
+    likeable_type { "Comment" }
+    liker { 1 }
   end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Like do
+  describe "factories" do
+    before :each do
+      @user = create(:user)
+      @liker = create(:user)
+      @post = create(:user_post, postable_id: @user.id, author_id: @user.id)
+    end
+    it "has a valid post_like factory" do
+      expect(build(:post_like, liker_id: @liker.id, likeable_id: @post.id)).to be_valid
+    end
+    it "has a valid comment_like factory" do
+      comment = create(:post_comment, author_id: create(:user).id, commentable_id: @post.id)
+      expect(build(:comment_like, liker_id: @liker.id, likeable_id: comment.id)).to be_valid
+    end
+  end
 end
