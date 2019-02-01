ec8635bbfb7ee62e0a297c745912ab9a57d3701b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-02-01 12:00:45 +0800

    Include dependent destroy to has_many assocations of post and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,8 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :author, class_name: 'User'
-  has_many :comments, as: :commentable
-  has_many :likes, as: :likeable
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :likes, as: :likeable, dependent: :destroy
   default_scope { order(created_at: :desc).eager_load(:author).eager_load(:likes) }
   validates :body, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :postable, polymorphic: true
-  has_many :comments, as: :commentable
-  has_many :likes, as: :likeable
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :likes, as: :likeable, dependent: :destroy
   default_scope { order(updated_at: :desc).eager_load(:author).eager_load(:likes).eager_load(:comments) }
   mount_uploader :post_pic, PostPicUploader
   validate :post_pic_size
