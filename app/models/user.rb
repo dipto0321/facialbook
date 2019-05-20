bbfd8fb0df19308671a4f99a7e82c8199ce9d2a4
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-05-20 14:07:38 +0800

    Correct style errors in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# User model
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
@@ -16,9 +17,9 @@ class User < ApplicationRecord
 
   has_many :likes, foreign_key: :liker_id, dependent: :destroy
 
-  has_many :active_friendships, class_name: 'Friendship', foreign_key: :user_id, dependent: :destroy
+  has_many :active_friendships, class_name: "Friendship", foreign_key: :user_id, dependent: :destroy
 
-  has_many :passive_friendships, class_name: 'Friendship', foreign_key: :friend_id, dependent: :destroy
+  has_many :passive_friendships, class_name: "Friendship", foreign_key: :friend_id, dependent: :destroy
 
   has_many :added_friends, through: :active_friendships,
                            source: :friend, dependent: :destroy
@@ -26,24 +27,25 @@ class User < ApplicationRecord
   has_many :adding_friends, through: :passive_friendships, source: :user, dependent: :destroy
 
   # All the request send by the user
-  has_many :active_requests, class_name: 'FriendRequest', foreign_key: :requester_id, dependent: :destroy
+  has_many :active_requests, class_name: "FriendRequest", foreign_key: :requester_id, dependent: :destroy
 
   # All the request received by the user
-  has_many :passive_requests, class_name: 'FriendRequest', foreign_key: :requestee_id, dependent: :destroy
+  has_many :passive_requests, class_name: "FriendRequest", foreign_key: :requestee_id, dependent: :destroy
 
   has_many :requestees, through: :active_requests
 
   has_many :requesters, through: :passive_requests
 
-  default_scope {
-                eager_load(:profile)
-                .eager_load(:active_requests)
-                .eager_load(:passive_requests)
-                .eager_load(:active_friendships)
-                .eager_load(:passive_friendships) }
+  default_scope do
+    eager_load(:profile)
+      .eager_load(:active_requests)
+      .eager_load(:passive_requests)
+      .eager_load(:active_friendships)
+      .eager_load(:passive_friendships)
+  end
 
   accepts_nested_attributes_for :profile, allow_destroy: true
-  
+
   before_validation :force_profile_creation
 
   def friends
@@ -55,56 +57,55 @@ class User < ApplicationRecord
   end
 
   def friendships
-    Friendship.where('user_id=? OR friend_id=?', id, id)
+    Friendship.where("user_id=? OR friend_id=?", id, id)
   end
 
   def pending_request?(requestee)
-    pending = active_requests.where('responded=? AND requestee_id=?', false, requestee.id)[0]
+    pending = active_requests.where("responded=? AND requestee_id=?", false, requestee.id)[0]
     return false if pending.nil?
 
     !pending.responded
   end
 
   def pending_passive_requests
-    passive_requests.where('responded=?', false)
+    passive_requests.where("responded=?", false)
   end
 
   def liked_post
-    Post.joins(:likes).where('liker_id=?', id)
+    Post.joins(:likes).where("liker_id=?", id)
   end
 
   def liked_comment
-    Comment.joins(:likes).where('liker_id=?', id)
+    Comment.joins(:likes).where("liker_id=?", id)
   end
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      if (data = session["devise.facebook_data"]) && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
     end
   end
-  
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      parse_name(user, auth.info.name)   # assuming the user model has a name
+      user.password = Devise.friendly_token[0, 20]
+      parse_name(user, auth.info.name) # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
     end
   end
 
   private
 
-  def self.parse_name(user, name)
+  def force_profile_creation
+    errors.add(:profile, :blank, message: "can't be nil") if profile.nil?
+  end
+
+  private_class_method def self.parse_name(user, name)
     name_arr = name.split(" ")
     user.last_name = name_arr.pop
     user.middle_name = name_arr.last
     user.first_name = name_arr.first
   end
-
-  def force_profile_creation
-    errors.add(:profile, :blank, message: "can't be nil") if profile.nil?
-  end
-
 end
