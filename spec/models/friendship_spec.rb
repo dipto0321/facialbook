1f1d7b509fa3871c7ae09b5505f22e6241a3e86d
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-05-27 16:33:06 +0800

    Complete friendship_spec with association specs

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -3,23 +3,50 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  
+
   let(:user) { create(:user) }
   let(:friend) { create(:user) }
-  
+
   before do
     @friendship = create(:friendship, user_id: user.id, friend_id: friend.id)
   end
 
-  context "before_save callback" do
+  describe "before_save callback" do
     it "concatenates ids of user and friend before save" do
       expect(@friendship.concatenated).to eq([user.id, friend.id].sort.insert(1, "X").join)
     end
   end
 
-  context ".find_friendship class method" do
+  describe ".find_friendship class method" do
     it "returns friendship between 2 users" do
       expect(Friendship.find_friendship(user, friend)).to eq(@friendship)
+    end
+  end
+
+  describe "associations" do
+    let(:friendship) { build(:friendship, user_id: user.id, friend_id: friend.id) }
+    describe "belongs_to user" do
+      context "user present" do
+        it "is valid" do
+          expect(friendship).to be_valid
+        end
+      end
+
+      context "no user" do
+        it "is invalid" do
+          friendship.user_id = nil
+          expect(friendship).to_not be_valid
+        end
+      end
+    end
+
+    describe "belongs_to friend" do
+      context "without friend" do
+        it("is invalid") do
+          friendship.friend_id = nil
+          expect(friendship).to_not be_valid
+        end
+      end
     end
   end
 end
