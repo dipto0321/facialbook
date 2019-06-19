b3441686f545989f6aa776d24b443c714408d5ba
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-06-19 13:01:58 +0800

    Remove columns that are dependent on user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_040537) do
+ActiveRecord::Schema.define(version: 2019_06_19_044539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -19,61 +19,46 @@ ActiveRecord::Schema.define(version: 2019_06_19_040537) do
   create_table "comments", force: :cascade do |t|
     t.string "commentable_type"
     t.bigint "commentable_id"
-    t.bigint "author_id"
     t.text "body"
     t.string "comment_pic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
-  create_table "friend_requests", force: :cascade do |t|
-    t.bigint "requester_id"
-    t.bigint "requestee_id"
+  create_table "friend_requests", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "concatenated"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["concatenated"], name: "index_friend_requests_on_concatenated", unique: true
-    t.index ["requestee_id"], name: "index_friend_requests_on_requestee_id"
-    t.index ["requester_id"], name: "index_friend_requests_on_requester_id"
   end
 
-  create_table "friendships", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "friend_id"
+  create_table "friendships", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "concatenated"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["concatenated"], name: "index_friendships_on_concatenated", unique: true
-    t.index ["friend_id"], name: "index_friendships_on_friend_id"
-    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.string "likeable_type"
     t.bigint "likeable_id"
-    t.bigint "liker_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable_type_and_likeable_id"
-    t.index ["liker_id"], name: "index_likes_on_liker_id"
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.text "body"
     t.string "post_pic"
     t.string "postable_type"
     t.bigint "postable_id"
-    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
     t.index ["postable_type", "postable_id"], name: "index_posts_on_postable_type_and_postable_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.bigint "user_id"
+  create_table "profiles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.datetime "birthday"
@@ -83,10 +68,9 @@ ActiveRecord::Schema.define(version: 2019_06_19_040537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "middle_name"
-    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -105,12 +89,4 @@ ActiveRecord::Schema.define(version: 2019_06_19_040537) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "users", column: "author_id"
-  add_foreign_key "friend_requests", "users", column: "requestee_id"
-  add_foreign_key "friend_requests", "users", column: "requester_id"
-  add_foreign_key "friendships", "users"
-  add_foreign_key "friendships", "users", column: "friend_id"
-  add_foreign_key "likes", "users", column: "liker_id"
-  add_foreign_key "posts", "users", column: "author_id"
-  add_foreign_key "profiles", "users"
 end
