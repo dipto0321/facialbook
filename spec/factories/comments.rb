7e7fef692460fb317d29b6c7844c3802821c733a
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-24 08:34:43 +0800

    Update arguments for Lorem.paragraph

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -5,13 +5,13 @@ FactoryBot.define do
     commentable_type { 'Post' }
     association :commentable, factory: :post
     association :author, factory: :user
-    body { Faker::Lorem.paragraph(2) }
+    body { Faker::Lorem.paragraph(sentence_count: 2) }
   end
 
   factory :comment_reply, class: 'Comment' do
     commentable_type { 'Comment' }
     association :commentable, factory: :comment
     association :author, factory: :user
-    body { Faker::Lorem.paragraph(2) }
+    body { Faker::Lorem.paragraph(sentence_count: 2) }
   end
 end
